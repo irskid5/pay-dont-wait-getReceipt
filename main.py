d5a39e7b23f14e190a6b4217e321dbeeb222a418
EC2 Default User <ec2-user@ip-172-31-45-197.us-east-2.compute.internal>
2020-04-14 18:35:22 +0000

    latest version

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def lambda_handler(event, context):
         items = {}
         for i in range(1,len(receipt)):
             #items[description] = [quantity, amount]
-            items[receipt[i][5]] = [receipt[i][6], receipt[i][7]]
+            items[receipt[i][5]] = {"maxNumber": receipt[i][6], "cost": receipt[i][7], "number": receipt[i][6], "total": receipt[i][6]*receipt[i][7]}
             
         print(items)
         
@@ -53,7 +53,7 @@ def lambda_handler(event, context):
                 "x-custom-header" : "my custom header value",
                 "Access-Control-Allow-Origin": "*"
             },
-            'body': json.dumps({"success": True, "total": total, "table_id": table_id, "service_id": service_id, "day_of_service": day_of_service, "service_started": service_started, "items_and_quantities_and_prices": items}, cls=DateTimeEncoder)
+            'body': json.dumps({"success": True, "total": total, "table_id": table_id, "service_id": service_id, "day_of_service": day_of_service, "service_started": service_started, "items": items}, cls=DateTimeEncoder)
         } 
     except Exception as err:
         print("Exception: " + str(err))
